bbdf4613743aea4db9816f27cdd18dd0160240f9
SankovaN <nadya_sankova@mail.ru>
2021-06-06 01:59:52 +0500

    Задание 2(4)

--- sandbox/src/main/java/training/sandbox/Point.java
+++ sandbox/src/main/java/training/sandbox/Point.java
@@ -3,4 +3,14 @@ package training.sandbox;
 public class Point {
     double x;
     double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point p2) {
+        double l = Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y));
+        return l;
+    }
 }
