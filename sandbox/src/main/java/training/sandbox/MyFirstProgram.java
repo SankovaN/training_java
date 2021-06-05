b0a3f9009bef83e0461ad4618745d83f9b2b048d
SankovaN <nadya_sankova@mail.ru>
2021-06-06 02:20:37 +0500

    Задание 2(4)

--- sandbox/src/main/java/training/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/training/sandbox/MyFirstProgram.java
@@ -4,20 +4,10 @@ public class MyFirstProgram {
 
 	public static void main(String[] args)
 	{
-		Point p1 = new Point();
-		Point p2 = new Point();
+		Point p1 = new Point(1, 1);
+		Point p2 = new Point(7, 7);
 
-		p1.x = 1;
-		p1.y = 1;
-		p2.x = 7;
-		p2.y = 7;
-
-		System.out.println("Расстояние между двумя точками = " + distance(p1, p2));
-	}
-
-	public static double distance(Point p1, Point p2) {
-		double l = Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
-		return l;
+		System.out.println("Расстояние между двумя точками = " + p1.distance(p2));
 	}
 
 }
