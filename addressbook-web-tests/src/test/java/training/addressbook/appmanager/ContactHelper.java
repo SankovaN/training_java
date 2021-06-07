8ec9c2d1353deed0d9491d142b529aca366f5362
SankovaN <nadya_sankova@mail.ru>
2021-06-07 18:03:33 +0500

    Задание 5, классы NavigationHelper, GroupHelper и ContactHelper

---
+++ addressbook-web-tests/src/test/java/training/addressbook/appmanager/ContactHelper.java
@@ -0,0 +1,48 @@
+package training.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import training.addressbook.model.ContactData;
+
+public class ContactHelper {
+    private WebDriver wd;
+
+    public ContactHelper(WebDriver wd) {
+        this.wd = wd;
+    }
+
+    public void gotoHomePage() {
+      wd.findElement(By.linkText("home page")).click();
+    }
+
+    public void submitContactForm() {
+      wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+    }
+
+    public void fillContactForm(ContactData contactData) {
+      wd.findElement(By.name("firstname")).click();
+      wd.findElement(By.name("firstname")).clear();
+      wd.findElement(By.name("firstname")).sendKeys(contactData.getName());
+      wd.findElement(By.name("lastname")).click();
+      wd.findElement(By.name("lastname")).clear();
+      wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+      wd.findElement(By.name("email")).click();
+      wd.findElement(By.name("email")).clear();
+      wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+      wd.findElement(By.name("address")).click();
+      wd.findElement(By.name("address")).clear();
+      wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
+      wd.findElement(By.name("home")).click();
+      wd.findElement(By.name("home")).clear();
+      wd.findElement(By.name("home")).sendKeys(contactData.getPhone());
+    }
+
+    public void deleteSelectedContact() {
+      wd.findElement(By.xpath("//input[@value='Delete']")).click();
+      wd.switchTo().alert().accept();
+    }
+
+    public void selectContact() {
+       wd.findElement(By.name("selected[]")).click();
+         }
+}
--- addressbook-web-tests/src/test/java/training/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/training/addressbook/tests/ContactCreationTests.java
@@ -8,10 +8,10 @@ public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() throws Exception {
-    app.gotoAddNew();
-    app.fillContactForm(new ContactData("testname", "lastname", "nadya_sankova@mail.ru", "test address", "81111111111"));
-    app.submitContactForm();
-    app.gotoHomePage();
+    app.getNavigationHelper().gotoAddNew();
+    app.getContactHelper().fillContactForm(new ContactData("testname", "lastname", "nadya_sankova@mail.ru", "test address", "81111111111"));
+    app.getContactHelper().submitContactForm();
+    app.getContactHelper().gotoHomePage();
     app.logout();
   }
 
