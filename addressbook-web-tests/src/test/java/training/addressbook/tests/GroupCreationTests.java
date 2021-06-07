f9cdfd7552c4190d5bc188abc239db78a3c2cdf9
SankovaN <nadya_sankova@mail.ru>
2021-06-08 03:37:38 +0500

    Задание 9, проверка для групп

--- addressbook-web-tests/src/test/java/training/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/training/addressbook/tests/GroupCreationTests.java
@@ -1,15 +1,30 @@
 package training.addressbook.tests;
 
-import org.testng.annotations.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import training.addressbook.model.GroupData;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
 
 public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    GroupData group = new GroupData("test2", "test2", "test3");
+    app.getGroupHelper().createGroup(group);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() + 1);
     //app.logout();
+
+    before.add(group);
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 
 }
