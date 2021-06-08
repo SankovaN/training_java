f25f24ba3242b59fef0101924d71d13880909a19
SankovaN <nadya_sankova@mail.ru>
2021-06-09 02:20:55 +0500

    Задание 10

--- addressbook-web-tests/src/test/java/training/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/training/addressbook/tests/ContactCreationTests.java
@@ -1,29 +1,24 @@
 package training.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import training.addressbook.model.ContactData;
+import training.addressbook.model.Contacts;
 
-import java.util.Comparator;
-import java.util.List;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-
     @Test
     public void testContactCreation() throws Exception {
-        List<ContactData> before = app.contact().list();
+        Contacts before = app.contact().all();
         ContactData contact = new ContactData().withName("testname").withLastname("lastname").withEmail("nadya_sankova@mail.ru").withAddress("test address").withPhone("81111111111");
         app.contact().create(contact);
-        List<ContactData> after = app.contact().list();
-        Assert.assertEquals(after.size(), before.size() + 1);
+        Contacts after = app.contact().all();
+        assertThat(after.size(), equalTo(before.size() + 1));
 
-        contact.withId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
-        before.add(contact);
-        Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
-        before.sort(byId);
-        after.sort(byId);
-        Assert.assertEquals(before, after);
+        assertThat(after, equalTo(
+                before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
     }
 
 }
