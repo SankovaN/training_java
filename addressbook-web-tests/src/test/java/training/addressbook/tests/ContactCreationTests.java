b982b5e9eb4330bde59990f25c6f5e2873d106dd
SankovaN <nadya_sankova@mail.ru>
2021-06-12 01:27:49 +0500

    Задание 13, контакты

--- addressbook-web-tests/src/test/java/training/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/training/addressbook/tests/ContactCreationTests.java
@@ -1,29 +1,45 @@
 package training.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import training.addressbook.model.ContactData;
 import training.addressbook.model.Contacts;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void testContactCreation() throws Exception {
-        Contacts before = app.contact().all();
-        File photo = new File("src/test/resources/test.jpg");
-        ContactData contact = new ContactData().withName("testname").withLastname("lastname")
-                .withEmail("nadya_sankova@mail.ru").withAddress("test address")
-                .withPhone("81111111111").withPhoto(photo);
-        app.contact().create(contact);
-        Contacts after = app.contact().all();
-        assertThat(after.size(), equalTo(before.size() + 1));
-
-        assertThat(after, equalTo(
-                before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+    @DataProvider
+    public Iterator<Object[]> validContacts() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File ("src/test/resources/contacts.xml")));
+        String xml = "";
+        String line = reader.readLine();
+        while(line != null) {
+            xml += line;
+            line = reader.readLine();
+        }
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        List<ContactData> contacts = (List<ContactData>)xstream.fromXML(xml);
+        return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
     }
 
+    @Test(dataProvider = "validContacts")
+    public void testContactCreation(ContactData contact) throws Exception {
+            Contacts before = app.contact().all();
+            app.contact().create(contact);
+            Contacts after = app.contact().all();
+            assertThat(after.size(), equalTo(before.size() + 1));
+
+            assertThat(after, equalTo(
+                    before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+        }
 }
