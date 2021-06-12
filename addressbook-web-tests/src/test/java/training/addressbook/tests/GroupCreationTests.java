7954cfdee5e54963bd6ed3c66d8307737734b0d3
SankovaN <nadya_sankova@mail.ru>
2021-06-12 16:06:44 +0500

    Промежуточные результаты

--- addressbook-web-tests/src/test/java/training/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/training/addressbook/tests/GroupCreationTests.java
@@ -1,6 +1,8 @@
 package training.addressbook.tests;
 
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import training.addressbook.model.GroupData;
--- addressbook-web-tests/src/test/java/training/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/training/addressbook/tests/TestBase.java
@@ -1,14 +1,23 @@
 package training.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import training.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
 
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
     protected static final ApplicationManager app
-            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+            = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
     @BeforeSuite(alwaysRun = true)
     public void setUp() throws Exception {
@@ -20,4 +29,13 @@ public class TestBase {
         app.stop();
     }
 
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m) {
+        logger.info("Stop test " + m.getName());
+    }
 }
