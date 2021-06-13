4a5f64197608c58c95f287c977cc1882c537f3b3
SankovaN <nadya_sankova@mail.ru>
2021-06-14 04:16:56 +0500

    Промежуточные результаты

---
+++ mantis-tests/src/test/resources/config_inc.php
@@ -0,0 +1,11 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'Europe/Berlin';
+
+$g_crypto_master_salt     = 'eAYiM5fY1AjHV7hBgWgZ1PJ3IquZdEBt4/lmclqPgro=';
+$g_signup_use_captcha     = OFF;
