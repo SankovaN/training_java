34fb6201e67138fc10f26df4e5ba86b6c0007724
SankovaN <nadya_sankova@mail.ru>
2021-06-13 05:56:04 +0500

    Задание 17

---
+++ mantis-tests/src/test/java/training/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,61 @@
+package training.mantis.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+    private final Properties properties;
+    protected WebDriver wd;
+
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+        properties = new Properties();
+    }
+
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+
+        if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE)) {
+            wd = new InternetExplorerDriver();
+        }
+
+        wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        wd.get(properties.getProperty("web.baseUrl"));
+        }
+
+    public void login(String username, String password) {
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//input[@value='Login']")).click();
+    }
+
+    public void logout() {
+        wd.findElement(By.linkText("Logout")).click();
+    }
+
+    public void stop() {
+        wd.quit();
+    }
+}
