86c624a88517c537dedcf972dba05bb1c4a47c20
SankovaN <nadya_sankova@mail.ru>
2021-06-15 00:34:30 +0500

    Промежуточные результаты

---
+++ mantis-tests/src/test/java/training/mantis/tests/ResetPasswordTests.java
@@ -0,0 +1,11 @@
+package training.mantis.tests;
+
+import org.testng.annotations.Test;
+
+public class ResetPasswordTests extends TestBase{
+
+   @Test
+    public void testResetPassword () {
+      
+   }
+}
