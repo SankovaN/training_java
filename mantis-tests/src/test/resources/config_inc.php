b6750063a57f871d49ffa5703471db42534ab635
SankovaN <nadya_sankova@mail.ru>
2021-06-14 05:58:39 +0500

    Промежуточные результаты

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -9,3 +9,6 @@ $g_default_timezone       = 'Europe/Berlin';
 
 $g_crypto_master_salt     = 'eAYiM5fY1AjHV7hBgWgZ1PJ3IquZdEBt4/lmclqPgro=';
 $g_signup_use_captcha     = OFF;
+
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
